bb4955171232a4e7079f8e7bfd4c58eb77a3577d
Christopher Kim <chris.kim1990@gmail.com>
2021-02-13 18:44:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+    #binding.pry
+    value.each do |inner_key, names|
+      binding.pry
+    end
+    final_array
+  end
+
 end
+  #binding.pry
